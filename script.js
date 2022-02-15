0b80305bb94fb46247fba5cce0d77a2347605313
Nikita Dachkovskii <noxik7777@gmail.com>
2022-02-15 17:00:49 +0200

    US001: Created Basic Markup

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        console.log(data);
+    })
+})
+
